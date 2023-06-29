b6c4a0c1f6907688d740ddc351661722cf6e13fc
Elmer Gonzalez <elmer.gonzalez@uip.edu.pa>
2023-06-28 22:52:23 -0500

    Fix get_meaning() function wrong result, adding if exists validation to editword() funciton, other changes.

--- main.py
+++ main.py
@@ -55,11 +55,22 @@ def insert_record(slang):
                        {'id': slang.id, 'word': slang.word, 'meaning': slang.meaning})
 
 
+def exist(value_requested):
+    with conn:
+        cursor.execute("SELECT id or word from SlangPanameno WHERE id = :id or word = :word",
+                       {'id': value_requested, 'word': value_requested})
+        result = cursor.fetchone()
+        if result is None:
+            return False
+        else:
+            return True
+
+
 # Edit records (2)
 def edit_record(word_request, new_word, new_meaning):
     with conn:
-        cursor.execute("UPDATE SlangPanameno SET word=:new_word, meaning=:new_meaning WHERE word = :word_request",
-                       {'word_request': word_request, 'new_meaning': new_meaning, 'new_word': new_word})
+        cursor.execute("UPDATE SlangPanameno SET word=:new_word, meaning=:new_meaning WHERE word = :word_request or id = :word_request ",
+                   {'word_request': word_request, 'new_meaning': new_meaning, 'new_word': new_word})
 
 
 # Delete record (3)
@@ -86,7 +97,7 @@ def get_meaning_by_word(word):
 cursor.execute("""
             insert into SlangPanameno(word, meaning)
             values 
-              ('Chombo', 'Amigo cercano o compañero'),
+              ('Chombo', 'Se usa para referirse a los afroantillanos y sus descendientes'),
               ('Jato', 'Casa o hogar'),
               ('Pana', 'Amigo o camarada'),
               ('Que xopa!', 'El clasico saludo de nosotros'),
@@ -120,11 +131,18 @@ def add_word():
 
 # Editing word function
 def edit_word():
-    word_request = input(f"{lb_t2}Ingrese palabra a editar: ")
-    new_word = input(f"{lb_t2}Ingrese nueva palabra: ")
-    new_meaning = input(f"{lb_t2}Ingrese el significado: ")
-    edit_record(word_request, new_word, new_meaning)
-    print(f"{lb_t2}¡Palabra editada!")
+    value_requested = input(f"{lb_t2}Presiones 0 para ver palabras{lb_t2}Ingrese el numero (#) o la palabra a editar: ")
+    if value_requested == "0":
+        see_words()
+        edit_word()
+    else:
+        if exist(value_requested):
+            new_word = input(f"{lb_t2}Ingrese nueva palabra: ")
+            new_meaning = input(f"{lb_t2}Ingrese el significado: ")
+            edit_record(value_requested, new_word, new_meaning)
+            print(f"{lb_t2}¡Palabra editada!")
+        else:
+            print(f"{lb_t2}ERROR: El valor ingresado no existe en la base de datos")
 
 
 # Deleting word function
@@ -146,7 +164,7 @@ def get_meaning():
     word = input(f"{lb_t}{tab}Ingresa la palabra a buscar: ")
     try:
         x = get_meaning_by_word(word)
-        print(f"{lb_t2}{x[0]}, significa: {x[1]}")
+        print(f"{lb_t2}{x[1]}, significa: {x[2]}")
     except TypeError:
         print(f"{lb_t2}ERROR - Palabra no encontrada")
 
@@ -171,7 +189,7 @@ menu = """
 
 # Program main loop // Warning! -> *** Need to use Python 3.10 for match - case Statements ***
 end = False
-len(menu)
+
 while not end:
     try:
         print(menu)
